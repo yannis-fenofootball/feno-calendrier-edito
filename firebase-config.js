1639ba536d291a860a04d59ce3d911667cb9ac1e
yannis-fenofootball <yannis@fenofootball.fr>
2026-09-19 13:08:14 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,9 @@
 // Configuration Firebase du calendrier Feno.
-// Remplace les valeurs ci-dessous par celles de ton projet Firebase
-// (Console Firebase > Paramètres du projet > Tes applications > icône Web).
-// Ces valeurs ne sont PAS secrètes : la sécurité réelle vient des règles
-// Firestore (firestore.rules), pas de la confidentialité de ce fichier.
 window.FENO_FIREBASE_CONFIG = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyDWF0S-6Z9_H7bintSjjoJ7XeKDaP8S-lw",
+  authDomain: "feno-calendrier-editorial.firebaseapp.com",
+  projectId: "feno-calendrier-editorial",
+  storageBucket: "feno-calendrier-editorial.firebasestorage.app",
+  messagingSenderId: "459574785958",
+  appId: "1:459574785958:web:11b2be10319fa4b2fa0167"
 };
